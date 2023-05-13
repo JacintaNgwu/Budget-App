746305590c8543cb0bf27784b81d0f57f7576e25
JacintaNgwu <jacintangwu9900@gmail.com>
2023-05-13 23:21:40 +0530

    Implement changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, allow_blank: false
+  validates :email, presence: true, allow_blank: false
+  validates :password, presence: true, allow_blank: false
   has_many :expenses, dependent: :destroy, foreign_key: :author_id
   has_many :categories, dependent: :destroy, foreign_key: :author_id
 end
